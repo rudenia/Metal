116793eadb85d52f37dded04cdc0491b4b779a0f
Ivan <ivanrudenia@gmail.com>
2020-11-02 08:51:38 +0100

    Small edits version 1.0.1

--- Metal/js/script.js
+++ Metal/js/script.js
@@ -13,5 +13,8 @@ $ (function () {
     $('.menu-open').click(function () {
         $('.menu-collapse').toggleClass('d-none')
         $('.menu-collapse').toggleClass('opened')
+        $('.hamburger-line__1').toggleClass('change1')
+        $('.hamburger-line__2').toggleClass('change2')
+        $('.hamburger-line__3').toggleClass('change3')
     })
 });
